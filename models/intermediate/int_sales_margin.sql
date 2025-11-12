94215fe640369132e47da9b63ff83219c9731b54
Imane Zampalegre <nadiazampalegre@gmail.com>
2025-11-12 00:04:32 +0000

    column margin and purchase cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,20 @@
+
+
+with s as (
+  select * from {{ ref('stg_gz_raw_data__raw_gz_sales') }}
+),
+p as (
+  select * from {{ ref('stg_gz_raw_data__raw_gz_product') }}
+)
+
+select
+  s.orders_id,
+  s.products_id,
+  s.quantity,
+  s.revenue,
+  p.purchase_price,
+  s.quantity * p.purchase_price        as purchase_cost,
+  s.revenue - (s.quantity * p.purchase_price) as margin
+from s
+left join p
+  on s.products_id = p.products_id
